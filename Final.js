c18f57eebd0e5bce556c97d2c5c7eb446e15d6f8
amyywong <54848525+amyywong@users.noreply.github.com>
2019-12-09 23:24:08 -0500

    final

---
+++ Final.js
@@ -0,0 +1,10 @@
+
+
+var n = document.getElementById('new');
+
+n.addEventListener("click", ChangeNewBg);
+
+function ChangeNewBg(){
+  n.style.backgroundColor = "#000000";
+  n.style.color = "white";
+}
